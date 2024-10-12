1346227ed323cda7bb7b7644523fc22f1d35ec05
dhiraj1996 <dhirajkumar1996@gmail.com>
2024-10-12 11:56:36 +0530

    notFound Page add

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Home from "./components/home";
 import AboutUs from "./components/about";
 import Users from "./components/users"
 import Navbar from "./components/navbar"
+import NotFound from "./components/notFound";
+import UserProfile from "./components/userProfile";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Navbar />}>
-        <Route exact path="/" element={<Home />} />
-        <Route path="/about" element={<AboutUs />} />
-        <Route path="/users" element={<Users/>} />
+          <Route exact path="/" element={<Home />} />
+          <Route path="/about" element={<AboutUs />} />
+          <Route path="/users" element={<Users/>} />
+          <Route path="/users/user/:username" element={<UserProfile/>} />
         </Route>
+        <Route path="*" element={<NotFound/>} />
         
 
       </Routes>
